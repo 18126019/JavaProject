9667cde5de96ca68e8e71ef29c00b329b0c239a8
duyuyen <duyuyenphanbinh@gmail.com>
2021-03-28 23:12:23 +0700

    update class 2

--- KUU/src/nha/DangBanNha.java
+++ KUU/src/nha/DangBanNha.java
@@ -1,6 +1,7 @@
 package nha;
 import java.sql.*;
 public class DangBanNha extends Nha {
+	protected int maNguoiDung;
     protected String ketCau;
     protected String hem;
     protected String matTien;
@@ -12,6 +13,7 @@ public class DangBanNha extends Nha {
     public DangBanNha()
     {
         super();
+        maNguoiDung = 0;
         ketCau = "";
         hem = "";
         matTien = "";
@@ -32,13 +34,51 @@ public class DangBanNha extends Nha {
     public int getID() {
     	return id;
     }
+    public int getMaNguoiDung()
+    {
+    	return this.maNguoiDung;
+    }
+    
+    public void setGhiChu(String ghiChu) {
+        this.ghiChu = ghiChu;
+    }
+    public void setHem(String hem) {
+        this.hem = hem;
+    }
+    public void setKetCau(String ketCau) {
+        this.ketCau = ketCau ;
+    }
+    public void setMatTien(String matTien) {
+        this.matTien = matTien;
+    }
+    public void setMaNguoiDung(int MaNguoiDung)
+    {
+    	this.maNguoiDung = MaNguoiDung;
+    }
     public void setID(int id) {
     	this.id = id;
     }
     
-    public void suaNha()
+    public void themDangBanNha(DangBanNha dbn)
     {
-        //
+    	/*try {
+			Statement statement = cnn.createStatement();
+			String insertSqlString = "Insert into NHADANGBAN(manguoidung, sotang, dientich, ketcau, giatien,sonha, duong, phuong, quan, ghichu, daxoa)" 
+			+ "values(" 
+			+ dbn.getMaNguoiDung() + ",'"
+			+ dbn.getSoTang() + "','" + this.daXoa + "')";
+			statement.executeUpdate(insertSqlString);
+			System.out.println("Add Sucessfully");
+			String add = "SELECT ID FROM HESOHEM WHERE ten = '" + hem.getTenHem() + "' AND heso = " + hem.getHesoHem();
+			ResultSet rs = statement.executeQuery(add);
+			while (rs.next()) {
+				hem.setID(rs.getInt("ID"));
+			}
+			cnn.commit();
+			statement.close();
+		} catch (SQLException e) {
+			System.out.println("Cannot insert student");
+		}*/
     }
     
   
