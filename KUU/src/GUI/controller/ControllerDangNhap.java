e63a165dc5fc34db4931671cd649e1d3517cc77f
duyuyen <duyuyenphanbinh@gmail.com>
2021-05-01 18:06:02 +0700

    add singleton

--- KUU/src/GUI/controller/ControllerDangNhap.java
+++ KUU/src/GUI/controller/ControllerDangNhap.java
@@ -3,20 +3,21 @@ package src.GUI.controller;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.fxml.*;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
-import javafx.event.ActionEvent;
 import javafx.stage.Stage;
 import src.BUS.nguoiDung.KhachHangBUS;
-import src.BUS.nguoiDung.NguoiDung;
-import src.BUS.nguoiDung.QuanTriBUS;
 import src.DTO.nguoiDung.KhachHangDTO;
+import src.application.java.UserSession;
 
-import java.io.*;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
@@ -31,18 +32,22 @@ public class ControllerDangNhap implements Initializable {
 	@FXML
 	private JFXButton chuyendk;
 
-
-	public void Login(){
+	public void Login() {
 		btndn.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent event) {
 				KhachHangBUS khachHangBUS = new KhachHangBUS();
 				ArrayList<KhachHangDTO> ds = khachHangBUS.danhSachKhachHang();
 				boolean flat = false;
-				for(int i = 0; i < ds.size(); i++)
-				{
-					if (tendn.getText().equals(ds.get(i).getTenDangNhap()) && matkhau.getText().equals(ds.get(i).getMatKhau())){
+				for (int i = 0; i < ds.size(); i++) {
+					if (tendn.getText().equals(ds.get(i).getTenDangNhap()) && matkhau.getText().equals(ds.get(i).getMatKhau())) {
 						flat = true;
+						UserSession userSession = UserSession.getInstace(ds.get(i).getTenDangNhap());
+						try {
+							switchToHome(event);
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
 					}
 				}
 				if (flat == true) {
@@ -58,10 +63,10 @@ public class ControllerDangNhap implements Initializable {
 		});
 	}
 
-	public void Chuyendk(){
+	public void Chuyendk() {
 		chuyendk.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
-			public void handle(ActionEvent event){
+			public void handle(ActionEvent event) {
 				Parent root = null;
 				try {
 					root = FXMLLoader.load(getClass().getResource("../../GUI/resources/fxml/dangky.fxml"));
@@ -70,18 +75,55 @@ public class ControllerDangNhap implements Initializable {
 				}
 				Scene scene = new Scene(root);
 
-				Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+				Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
 				window.setScene(scene);
 				window.show();
 			}
-
 		});
 	}
+
+
 	@Override
 	public void initialize(URL url, ResourceBundle resourceBundle) {
 		Login(); // dang nhap
 		Chuyendk(); // chuyen qua man hinh dang ky
 	}
 
+	public void switchToHome(ActionEvent event) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("../../GUI/resources/fxml/home.fxml"));
+		Scene scene = new Scene(root);
+		Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		stage.setScene(scene);
+		stage.show();
+
+	}
+	public void switchToSetting(ActionEvent event) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("../../GUI/resources/fxml/setting.fxml"));
+		Scene scene = new Scene(root);
+		Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		stage.setScene(scene);
+		stage.show();
+	}
+	public void switchToDinhGia(ActionEvent event) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("../../GUI/resources/fxml/dinhgia.fxml"));
+		Scene scene = new Scene(root);
+		Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		stage.setScene(scene);
+		stage.show();
+	}
+	public void switchToSearch(ActionEvent event) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("../../GUI/resources/fxml/timkiem.fxml"));
+		Scene scene = new Scene(root);
+		Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		stage.setScene(scene);
+		stage.show();
+	}
+	public void switchToAddHome(ActionEvent event) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("../../GUI/resources/fxml/themNha.fxml"));
+		Scene scene = new Scene(root);
+		Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		stage.setScene(scene);
+		stage.show();
+	}
 
 }
--- KUU/src/application/java/Main.java
+++ KUU/src/application/java/Main.java
@@ -6,16 +6,20 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import src.GUI.controller.ControllerDangNhap;
+import src.GUI.controller.ControllerDetail;
 
 public class Main extends Application {
 	@Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("../../GUI/resources/fxml/home.fxml"));
-
+        Parent root = FXMLLoader.load(getClass().getResource("../../GUI/resources/fxml/login.fxml"));
         primaryStage.setScene(new Scene(root, 1280, 720));
         Image icon = new Image(getClass().getResourceAsStream("../../GUI/resources/img/logo.jpg"));
         primaryStage.getIcons().add(icon);
         primaryStage.setTitle("KUU");
         primaryStage.show();
+
+
+
     }
 }
